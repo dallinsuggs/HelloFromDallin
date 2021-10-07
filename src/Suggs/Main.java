0b85fc59fe306641e8529e5ea34ee5521beb5bdd
Dallin Suggs <dallin.suggs@gmail.com>
2021-10-07 08:52:10 -0600

    Initial commit

---
+++ src/Suggs/Main.java
@@ -0,0 +1,10 @@
+package Suggs;
+
+public class Main {
+
+    static String myName = "Dallin";
+
+    public static void main(String[] args) {
+	    System.out.format("Hello, my name is %s.", myName);
+    }
+}
