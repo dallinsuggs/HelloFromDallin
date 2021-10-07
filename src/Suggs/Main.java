064468b37fd205709336925f93ed44ed9b4a8672
garywilki <wil20223@byui.edu>
2021-10-07 15:04:55 -0700

    Step 6

--- src/Suggs/Main.java
+++ src/Suggs/Main.java
@@ -2,9 +2,10 @@ package Suggs;
 
 public class Main {
 
-    static String myName = "Dallin";
+    static String myName = "Dallin", hisName = "Gary";
 
     public static void main(String[] args) {
 	    System.out.format("Hello, my name is %s.", myName);
+        System.out.format("\nHello %s, my name is %s.", myName, hisName);
     }
 }
